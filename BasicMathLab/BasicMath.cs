05928445aa08708b54247718ae2e5f7992c7956e
Minh Tam Nguyen <tamnguyen277353@gmail.com>
2025-03-17 11:34:25 -0600

    Add project files.

---
+++ BasicMathLab/BasicMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMathLab
+{
+    public class BasicMath
+    {
+        public double Add(double x, double y) { return x + y; }
+
+        public double Subtract(double x, double y) { return x - y; }
+
+        public double Multiply(double x, double y) { return (x * y); }
+
+        public double Divide(double x, double y) {return (x / y); }
+    }
+
+}
---
+++ BasicMathTest/MathTests.cs
@@ -0,0 +1,56 @@
+﻿using BasicMathLab;
+
+namespace BasicMathTest
+{
+    public class MathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(2 , 3, 5)]
+        public void Test_AddMethod(double input1, double input2, double expected)
+        {
+            BasicMath basicMath = new BasicMath();
+
+            double actual = basicMath.Add(input1, input2);
+
+            Assert.That(expected, Is.EqualTo(actual).Within(0.001));
+        }
+
+        [Test]
+        [TestCase(25, 5, 5)]
+        public void Test_DivideMethod(double input1, double input2, double expected)
+        {
+            BasicMath basicMath = new BasicMath();
+
+            double actual = basicMath.Divide(input1, input2);
+
+            Assert.That(expected, Is.EqualTo(actual).Within(0.001));
+        }
+
+        [Test]
+        [TestCase(4, 5, 20)]
+        public void Test_MutiplyMethod(double input1, double input2, double expected) 
+        {
+            BasicMath basicMath = new BasicMath();
+            
+            double actual = basicMath.Multiply(input1, input2);
+
+            Assert.That(expected, Is.EqualTo(actual).Within(0.001));
+        }
+
+        [Test]
+        [TestCase(10, 11, -1)]
+        public void Test_SubtractMethod(double input1, double input2, double expected) 
+        {
+            BasicMath basicMath = new BasicMath();
+            
+            double actual = basicMath.Subtract(input1, input2);
+
+            Assert.That(expected, Is.EqualTo(actual).Within(0.001));
+        }
+    }
+}
